af357cb22e036edf75523a30f605dd7983c781a1
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-03-29 15:12:27 +0530

    Add files via upload

---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class LearnDatatypes {
+	
+public static void main(String[] args) {
+	   //byte
+	   byte age=30;
+	   System.out.println(age);
+	   
+	   //short
+	   short ageOfPerson=128;
+	   short amount=30000;
+	   
+	   //int
+	   int depositAmount=50000;
+	   long mobileNumber=8925411170L;
+	   
+	   //float
+	   float rateOfInterest=7.5F;
+	   
+	   //double
+	   float rupee=1.123456789F;
+	   double usd=1.123456789;
+	   System.out.println(rupee);
+	   System.out.println(usd);
+	   
+	   //boolean
+	   boolean haveBreakfast=false;
+	   
+	   //char
+	    char logo='T';
+	   
+	   //String
+	   String name="Vineeth";
+	}
+
+}
